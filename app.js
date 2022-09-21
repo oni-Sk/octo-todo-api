a5e381515373f3ce62a7573f118aeea8c83736e3
Malek Zemni <malek.zemni@octo.com>
2022-09-21 13:30:10 +0200

    supprimer code

--- app.js
+++ app.js
@@ -10,26 +10,6 @@ app.get("/", function (req, res) {
   res.send(`Hello World! Environnement de ${process.env.ENV}`)
 })
 
-app.get("/todos", async function (req, res) {
-  const todos = (await sequelize.query("SELECT * FROM todos"))[0]
-  res.send(todos)
-})
-
-app.post("/todos", async function (req, res) {
-  console.log(
-    `Création d'un ToDo avec les données : ${JSON.stringify(req.body)}`
-  )
-
-  await sequelize.query(
-    `INSERT INTO todos(description, date_echeance) VALUES(?, ?) RETURNING id`,
-    {
-      replacements: [req.body.description, req.body.date]
-    }
-  )
-
-  res.send("Ok")
-})
-
 const port = process.env.PORT
 app.listen(port, function () {
   console.log(`ToDo API listening on port ${port}`)
