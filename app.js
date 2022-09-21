7633b6bea689f1decec3aa5ee0552a218210c6af
Sonny Klotz <klotz.sonny@gmail.com>
2022-09-21 15:47:01 +0200

    feat: get et post todos

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ app.get("/", function (req, res) {
   res.send(`Hello World! Environnement de ${process.env.ENV}`)
 })
 
+app.get("/todos", async function (req, res) {
+  const todos = await sequelize.query("SELECT * FROM todos")
+  res.send(todos[0])
+})
+
+app.post("/todos", async function(req,res) {
+  await sequelize.query(`INSERT INTO todos(description, date_echeance) VALUES(?, ?) RETURNING id`, {
+    replacements: [req.body.description, req.body.date]
+  })
+  res.send("Ok")
+})
+
 const port = process.env.PORT
 app.listen(port, function () {
   console.log(`ToDo API listening on port ${port}`)
